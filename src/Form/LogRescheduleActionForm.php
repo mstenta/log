19a67aa022b5e20abb3903dcb07ab5f7d0b43619
pcambra <pcambra@122101.no-reply.drupal.org>
2020-05-21 08:53:45 -0400

    Issue #2599162 by pcambra: Relative log rescheduling

--- src/Form/LogRescheduleActionForm.php
+++ src/Form/LogRescheduleActionForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\log\Form;
 
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -40,15 +41,110 @@ class LogRescheduleActionForm extends LogActionFormBase {
   /**
    * {@inheritdoc}
    */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+
+    $form['type_of_date'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Reschedule by a relative date'),
+      '#weight' => -10,
+    ];
+    $form['title'] = [
+      '#type' => 'html_tag',
+      '#tag' => 'h4',
+      '#value' => $form['date']['#title'],
+      '#weight' => -9,
+    ];
+
+    // Datetime fields need to be wrapped for #states to work.
+    // @see https://www.drupal.org/project/drupal/issues/2419131
+    $form['absolute'] = [
+      '#type' => 'container',
+      '#states' => [
+        'visible' => [
+          ':input[name="type_of_date"]' => ['checked' => FALSE],
+        ],
+      ],
+    ];
+    $form['absolute']['date'] = $form['date'];
+    unset($form['absolute']['date']['#title']);
+    $form['absolute']['date']['#required'] = FALSE;
+    unset($form['date']);
+
+    $form['relative'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => ['container-inline'],
+      ],
+      '#states' => [
+        'visible' => [
+          ':input[name="type_of_date"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+    $form['relative']['amount'] = [
+      '#type' => 'number',
+      '#size' => 4,
+    ];
+    $form['relative']['time'] = [
+      '#type' => 'select',
+      '#options' => [
+        'hour' => $this->t('Hours'),
+        'day' => $this->t('Days'),
+        'week' => $this->t('Weeks'),
+        'month' => $this->t('Months'),
+        'year' => $this->t('Years'),
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $type_of_date = $form_state->getValue('type_of_date');
+    if ($type_of_date) {
+      $amount = $form_state->getValue('amount');
+      $time = $form_state->getValue('time');
+      if (empty($amount)) {
+        $form_state->setError($form['relative']['amount'], 'Please enter the amount of time for rescheduling.');
+      }
+      if (empty($time)) {
+        $form_state->setError($form['relative']['amount'], 'Please enter the time units for rescheduling.');
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     if ($form_state->getValue('confirm') && !empty($this->logs)) {
-      /** @var \Drupal\Core\Datetime\DrupalDateTime $new_date */
-      $new_date = $form_state->getValue('date');
       $count = count($this->logs);
-      foreach ($this->logs as $log) {
-        $log->get('status')->first()->applyTransitionById('to_pending');
-        $log->set('timestamp', $new_date->getTimestamp());
-        $log->save();
+      $type_of_date = $form_state->getValue('type_of_date');
+      if ($type_of_date) {
+        $amount = $form_state->getValue('amount');
+        $time = $form_state->getValue('time');
+        $sign = ($amount >= 0) ? '+' : '';
+        foreach ($this->logs as $log) {
+          $new_date = new DrupalDateTime();
+          $new_date->setTimestamp($log->get('timestamp')->value);
+          $new_date->modify("$sign$amount $time");
+          $log->get('status')->first()->applyTransitionById('to_pending');
+          $log->set('timestamp', $new_date->getTimestamp());
+          $log->save();
+        }
+      }
+      else {
+        /** @var \Drupal\Core\Datetime\DrupalDateTime $new_date */
+        $new_date = $form_state->getValue('date');
+        foreach ($this->logs as $log) {
+          $log->get('status')->first()->applyTransitionById('to_pending');
+          $log->set('timestamp', $new_date->getTimestamp());
+          $log->save();
+        }
       }
       $this->messenger()->addMessage($this->formatPlural($count, 'Rescheduled 1 log.', 'Rescheduled @count logs.'));
     }
--- tests/src/Functional/LogActionsTest.php
+++ tests/src/Functional/LogActionsTest.php
@@ -2,9 +2,6 @@
 
 namespace Drupal\Tests\log\Functional;
 
-use Drupal\Core\Datetime\DrupalDateTime;
-use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
-
 /**
  * Tests the Log form actions.
  *
@@ -108,9 +105,9 @@ class LogActionsTest extends LogTestBase {
   }
 
   /**
-   * Tests rescheduling a single log.
+   * Tests rescheduling a single log to an absolute date.
    */
-  public function testRescheduleSingleLog() {
+  public function testRescheduleSingleLogAbsolute() {
     $log = $this->createLogEntity([
       'name' => $this->randomMachineName(),
       'created' => \Drupal::time()->getRequestTime(),
@@ -130,11 +127,11 @@ class LogActionsTest extends LogTestBase {
     $this->assertText($this->t('Are you sure you want to reschedule this log?'));
     $this->assertText($this->t('New date'));
 
-    $edit_clone = [];
-    $edit_clone['date[month]'] = 01;
-    $edit_clone['date[year]'] = 2037;
-    $edit_clone['date[day]'] = 01;
-    $this->drupalPostForm(NULL, $edit_clone, $this->t('Reschedule'));
+    $edit_reschedule = [];
+    $edit_reschedule['date[month]'] = 01;
+    $edit_reschedule['date[year]'] = 2037;
+    $edit_reschedule['date[day]'] = 01;
+    $this->drupalPostForm(NULL, $edit_reschedule, $this->t('Reschedule'));
     $this->assertResponse(200);
     $this->assertUrl('admin/content/log');
     $this->assertText($this->t('Rescheduled 1 log'));
@@ -147,9 +144,9 @@ class LogActionsTest extends LogTestBase {
   }
 
   /**
-   * Tests rescheduling multiple logs.
+   * Tests rescheduling multiple logs to an absolute date.
    */
-  public function testRescheduleMultipleLogs() {
+  public function testRescheduleMultipleLogsAbsolute() {
     $timestamps = [
       386121600,
       286121600,
@@ -157,13 +154,12 @@ class LogActionsTest extends LogTestBase {
     ];
     $expected_timestamps = [];
     foreach ($timestamps as $timestamp) {
-      $formatted_date = DrupalDateTime::createFromTimestamp($timestamp, DateTimeItemInterface::STORAGE_TIMEZONE)->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT);
-      $expected_timestamps[] = $formatted_date;
+      $expected_timestamps[] = $timestamp;
       $log = $this->createLogEntity([
         'name' => $this->randomMachineName(),
         'created' => \Drupal::time()->getRequestTime(),
         'done' => TRUE,
-        'timestamp' => $formatted_date,
+        'timestamp' => $timestamp,
       ]);
       $log->save();
     }
@@ -181,11 +177,11 @@ class LogActionsTest extends LogTestBase {
     $this->assertText($this->t('Are you sure you want to reschedule these logs?'));
     $this->assertText($this->t('New date'));
 
-    $edit_clone = [];
-    $edit_clone['date[month]'] = 01;
-    $edit_clone['date[year]'] = 2037;
-    $edit_clone['date[day]'] = 01;
-    $this->drupalPostForm(NULL, $edit_clone, $this->t('Reschedule'));
+    $edit_reschedule = [];
+    $edit_reschedule['date[month]'] = 01;
+    $edit_reschedule['date[year]'] = 2037;
+    $edit_reschedule['date[day]'] = 01;
+    $this->drupalPostForm(NULL, $edit_reschedule, $this->t('Reschedule'));
     $this->assertResponse(200);
     $this->assertUrl('admin/content/log');
     $this->assertText($this->t('Rescheduled 3 logs'));
@@ -196,6 +192,107 @@ class LogActionsTest extends LogTestBase {
       $this->assertEqual($log->get('timestamp')->value, '2114341200', 'Timestamp on the log has changed.');
       $this->assertEqual($log->get('status')->value, 'pending', 'Log has been set to pending.');
     }
+  }
+
+  /**
+   * Tests rescheduling a single log to an relative date.
+   */
+  public function testRescheduleSingleLogRelative() {
+    $log = $this->createLogEntity([
+      'name' => $this->randomMachineName(),
+      'created' => \Drupal::time()->getRequestTime(),
+      'done' => TRUE,
+      'timestamp' => 386121600,
+    ]);
+    $log->save();
+
+    $num_of_logs = $this->storage->getQuery()->count()->execute();
+    $this->assertEqual($num_of_logs, 1, 'There is one log in the system.');
+
+    $edit = [];
+    $edit['action'] = 'log_reschedule_action';
+    $edit['entities[1]'] = TRUE;
+    $this->drupalPostForm('admin/content/log', $edit, $this->t('Apply to selected items'));
+    $this->assertResponse(200);
+    $this->assertText($this->t('Are you sure you want to reschedule this log?'));
+    $this->assertText($this->t('New date'));
+
+    $edit_reschedule = [];
+    $edit_reschedule['type_of_date'] = 1;
+    $this->drupalPostForm(NULL, $edit_reschedule, $this->t('Reschedule'));
+    $this->assertResponse(200);
+    $this->assertUrl('admin/content/log/reschedule');
+    $this->assertText($this->t('Please enter the amount of time for rescheduling.'));
+
+    $edit_reschedule = [];
+    $edit_reschedule['type_of_date'] = 1;
+    $edit_reschedule['amount'] = 1;
+    $edit_reschedule['time'] = 'day';
+    $this->drupalPostForm(NULL, $edit_reschedule, $this->t('Reschedule'));
+    $this->assertResponse(200);
+    $this->assertUrl('admin/content/log');
+    $this->assertText($this->t('Rescheduled 1 log'));
+
+    $logs = $this->storage->loadMultiple();
+    $this->assertEqual($num_of_logs, 1, 'There is one log in the system.');
+    $log = reset($logs);
+    $this->assertEqual($log->get('timestamp')->value, '386208000', 'Timestamp on the log has changed.');
+    $this->assertEqual($log->get('status')->value, 'pending', 'Log has been set to pending.');
+  }
+
+  /**
+   * Tests rescheduling multiple logs to an relative date.
+   */
+  public function testRescheduleMultipleLogsRelative() {
+    $timestamps = [
+      386121600,
+      286121600,
+      186121600,
+    ];
+    $expected_timestamps = [
+      383702400,
+      283443200,
+      183446800,
+    ];
+    foreach ($timestamps as $timestamp) {
+      $log = $this->createLogEntity([
+        'name' => $this->randomMachineName(),
+        'created' => \Drupal::time()->getRequestTime(),
+        'done' => TRUE,
+        'timestamp' => $timestamp,
+      ]);
+      $log->save();
+    }
+
+    $num_of_logs = $this->storage->getQuery()->count()->execute();
+    $this->assertEqual($num_of_logs, 3, 'There are three logs in the system.');
+
+    $edit = [];
+    $edit['action'] = 'log_reschedule_action';
+    for ($i = 1; $i <= 3; $i++) {
+      $edit['entities[' . $i . ']'] = TRUE;
+    }
+    $this->drupalPostForm('admin/content/log', $edit, $this->t('Apply to selected items'));
+    $this->assertResponse(200);
+    $this->assertText($this->t('Are you sure you want to reschedule these logs?'));
+    $this->assertText($this->t('New date'));
+
+    $edit_reschedule = [];
+    $edit_reschedule['type_of_date'] = 1;
+    $edit_reschedule['amount'] = -1;
+    $edit_reschedule['time'] = 'month';
+    $this->drupalPostForm(NULL, $edit_reschedule, $this->t('Reschedule'));
+    $this->assertResponse(200);
+    $this->assertUrl('admin/content/log');
+    $this->assertText($this->t('Rescheduled 3 logs'));
+
+    $logs = $this->storage->loadMultiple();
+    $this->assertEqual(count($logs), 3, 'There are three logs in the system.');
+    $log_timestamps = [];
+    foreach ($logs as $log) {
+      $log_timestamps[] = $log->get('timestamp')->value;
+    }
+    $this->assertEqual($log_timestamps, $expected_timestamps, 'Logs have been rescheduled');
   }
 
 }
