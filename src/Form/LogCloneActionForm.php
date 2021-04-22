b08fee7c5c7a953b341d9364ccabe19775456697
paul121 <paul.weidner@gmail.com>
2021-04-22 08:04:53 -0700

    Require view and create permissions for the log clone action.

--- src/Form/LogCloneActionForm.php
+++ src/Form/LogCloneActionForm.php
@@ -41,16 +41,35 @@ class LogCloneActionForm extends LogActionFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    // Filter out logs the user doesn't have access to.
+    $inaccessible_logs = [];
+    $accessible_logs = [];
+    $current_user = $this->currentUser();
+    foreach ($this->logs as $log) {
+      if (!$log->access('view', $current_user) || !$log->access('create', $current_user)) {
+        $inaccessible_logs[] = $log;
+        continue;
+      }
+      $accessible_logs[] = $log;
+    }
+
     /** @var \Drupal\Core\Datetime\DrupalDateTime $new_date */
-    if ($form_state->getValue('confirm') && !empty($this->logs)) {
+    if ($form_state->getValue('confirm') && !empty($accessible_logs)) {
       $new_date = $form_state->getValue('date');
       $count = count($this->logs);
-      foreach ($this->logs as $log) {
+      foreach ($accessible_logs as $log) {
         $cloned_log = $log->createDuplicate();
         $cloned_log->set('timestamp', $new_date->getTimestamp());
         $cloned_log->save();
       }
       $this->messenger()->addMessage($this->formatPlural($count, 'Cloned 1 log.', 'Cloned @count logs.'));
+    }
+
+    // Add warning message if there were inaccessible logs.
+    if (!empty($inaccessible_logs)) {
+      $inaccessible_count = count($inaccessible_logs);
+      $this->messenger()->addWarning($this->formatPlural($inaccessible_count, 'Could not clone @count log because you do not have the necessary permissions.', 'Could not clone @count logs because you do not have the necessary permissions.'));
     }
 
     parent::submitForm($form, $form_state);
