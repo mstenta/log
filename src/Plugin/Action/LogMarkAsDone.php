857b5d0d97a7570ccda5e2f6280933591a49153f
paul121 <paul.weidner@gmail.com>
2021-10-18 13:37:47 -0700

    Log state change actions support any workflow with the target state.

--- src/Plugin/Action/LogMarkAsDone.php
+++ src/Plugin/Action/LogMarkAsDone.php
@@ -2,10 +2,6 @@
 
 namespace Drupal\log\Plugin\Action;
 
-use Drupal\Core\Action\ActionBase;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\log\Entity\LogInterface;
-
 /**
  * Action that marks a log as done.
  *
@@ -15,28 +11,11 @@ use Drupal\log\Entity\LogInterface;
  *   type = "log"
  * )
  */
-class LogMarkAsDone extends ActionBase {
+class LogMarkAsDone extends LogStateChangeBase {
 
   /**
    * {@inheritdoc}
    */
-  public function execute(LogInterface $log = NULL) {
-    if ($log) {
-      $log->get('status')->first()->applyTransitionById('done');
-      $log->setNewRevision(TRUE);
-      $log->save();
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
-    /** @var \Drupal\log\Entity\LogInterface $object */
-    $result = $object->get('status')->access('edit', $account, TRUE)
-      ->andIf($object->access('update', $account, TRUE));
-
-    return $return_as_object ? $result : $result->isAllowed();
-  }
+  protected $targetState = 'done';
 
 }
--- src/Plugin/Action/LogMarkAsPending.php
+++ src/Plugin/Action/LogMarkAsPending.php
@@ -2,10 +2,6 @@
 
 namespace Drupal\log\Plugin\Action;
 
-use Drupal\Core\Action\ActionBase;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\log\Entity\LogInterface;
-
 /**
  * Action that marks a log as pending.
  *
@@ -15,28 +11,11 @@ use Drupal\log\Entity\LogInterface;
  *   type = "log"
  * )
  */
-class LogMarkAsPending extends ActionBase {
+class LogMarkAsPending extends LogStateChangeBase {
 
   /**
    * {@inheritdoc}
    */
-  public function execute(LogInterface $log = NULL) {
-    if ($log) {
-      $log->get('status')->first()->applyTransitionById('to_pending');
-      $log->setNewRevision(TRUE);
-      $log->save();
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
-    /** @var \Drupal\log\Entity\LogInterface $object */
-    $result = $object->get('status')->access('edit', $account, TRUE)
-      ->andIf($object->access('update', $account, TRUE));
-
-    return $return_as_object ? $result : $result->isAllowed();
-  }
+  protected $targetState = 'pending';
 
 }
