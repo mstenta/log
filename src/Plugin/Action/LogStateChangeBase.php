c60484fa790b46ea537c30118a6c1da81c13aee0
Paul Weidner <paul.weidner@gmail.com>
2021-12-08 10:48:52 -0500

    Issue #3243667 by paul121: Perform entity validation in bulk actions

--- src/Plugin/Action/LogStateChangeBase.php
+++ src/Plugin/Action/LogStateChangeBase.php
@@ -35,6 +35,21 @@ abstract class LogStateChangeBase extends EntityActionBase {
     if ($state_item->getOriginalId() !== $this->targetState && $transition = $state_item->getWorkflow()->findTransition($state_item->getOriginalId(), $this->targetState)) {
       $state_item->applyTransition($transition);
       $log->setNewRevision(TRUE);
+
+      // Validate the entity before saving.
+      $violations = $log->validate();
+      if ($violations->count() > 0) {
+        $this->messenger()->addWarning(
+          $this->t('Could not change the status of <a href=":entity_link">%entity_label</a>: validation failed.',
+            [
+              ':entity_link' => $log->toUrl()->setAbsolute()->toString(),
+              '%entity_label' => $log->label(),
+            ],
+          ),
+        );
+        return;
+      }
+
       $log->save();
     }
   }
