c18125e46e31cf7db70a4813d8f5467db9b9728a
Michael Stenta <mike@mstenta.net>
2023-11-27 10:43:11 -0500

    Add default view_builder handler for log type.
    See https://github.com/farmOS/farmOS/pull/752

--- src/Entity/LogType.php
+++ src/Entity/LogType.php
@@ -20,6 +20,7 @@ use Drupal\Core\Entity\EntityStorageInterface;
  *   ),
  *   handlers = {
  *     "list_builder" = "Drupal\log\LogTypeListBuilder",
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
  *     "form" = {
  *       "add" = "Drupal\log\Form\LogTypeForm",
  *       "edit" = "Drupal\log\Form\LogTypeForm",
