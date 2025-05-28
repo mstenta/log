66c6c9faee359ec5eb4064ea9b029f3fcae0bb41
Michael Stenta <mike@mstenta.net>
2025-05-28 13:45:24 -0400

    PHPStan level 2 fix: Call to an undefined method Drupal\log\Entity\LogTypeInterface::getWorkflowId().

--- src/Entity/LogTypeInterface.php
+++ src/Entity/LogTypeInterface.php
@@ -12,6 +12,26 @@ use Drupal\Core\Entity\RevisionableEntityBundleInterface;
 interface LogTypeInterface extends ConfigEntityInterface, EntityDescriptionInterface, RevisionableEntityBundleInterface {
 
   /**
+   * Gets the log type's workflow ID.
+   *
+   * Used by the $log->status field.
+   *
+   * @return string
+   *   The log type workflow ID.
+   */
+  public function getWorkflowId();
+
+  /**
+   * Sets the workflow ID of the log type.
+   *
+   * @param string $workflow_id
+   *   The workflow ID.
+   *
+   * @return $this
+   */
+  public function setWorkflowId($workflow_id);
+
+  /**
    * Returns the name pattern for a log type.
    *
    * @return string
